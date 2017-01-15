3c5da39a5d4019253b400dada9c0f3338ae0cee3
johngarcia9110 <johngarcia9110@gmail.com>
2017-01-15 10:06:00 -0500

    adjusted header to include bootstrap styles, removed sidebar from pages

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,24 @@
-<header class="banner">
+<header>
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerMain" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+        <?php
+        // Use the new walker
+        wp_nav_menu([
+           'menu'            => 'primary_navigation',
+           'theme_location'  => 'primary_navigation',
+           'container'       => 'div',
+           'container_id'    => 'navbarTogglerMain',
+           'container_class' => 'collapse navbar-collapse pull-xs-right',
+           'menu_id'         => false,
+           'menu_class'      => 'nav navbar-nav',
+           'depth'           => 2,
+           'fallback_cb'     => 'bs4navwalker::fallback',
+           'walker'          => new bs4navwalker()
+        ]);?>
     </nav>
   </div>
 </header>
