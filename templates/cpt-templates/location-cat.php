cddda53fd31596bbb47441ec89b15c8a718c021e
johngarcia9110 <johngarcia9110@gmail.com>
2017-01-22 16:51:51 -0500

    updated stylesheets for state icons

--- templates/cpt-templates/location-cat.php
+++ templates/cpt-templates/location-cat.php
@@ -7,12 +7,12 @@
     if ( $terms && !is_wp_error( $terms ) ) : ?>
       <?php foreach ( $terms as $term ) { ?>
         <div class="col-sm-3">
-          <div class="state-card">
-            <div class="state-icon">
-              <i class="state-icon <?php echo 'state-' . strtolower($term->name); ?>"></i>
+            <div class="state-card text-center" onclick="location.href='<?php echo get_term_link($term->slug, $taxonomy); ?>';">
+              <div class="state-icon-wrapper">
+                <i class="state-icon <?php echo 'state-' . strtolower($term->name); ?>"></i>
+              </div>
+              <h2><a href="<?php echo get_term_link($term->slug, $taxonomy); ?>"><?php echo $term->name; ?></a></h2>
             </div>
-          <h2><a href="<?php echo get_term_link($term->slug, $taxonomy); ?>"><?php echo $term->name; ?></a></h2>
-          </div>
         </div>
       <?php } ?>
   <?php endif;?>
